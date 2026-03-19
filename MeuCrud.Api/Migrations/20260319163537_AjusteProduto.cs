736efa3551c32f655057f2f977c4e0cdc1e4fc15
guilhermepierri2022 <guilhermepierri@hotmail.com>
2026-03-19 16:00:24 -0300

    Primeiro commit

---
+++ MeuCrud.Api/Migrations/20260319163537_AjusteProduto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuCrud.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Produtos",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantidade",
+                table: "Produtos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Produtos");
+        }
+    }
+}
